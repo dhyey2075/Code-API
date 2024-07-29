30062bc5b53385355af297a1594d06577614e2d4
dhyey2075 <dhyeyhparekh@gmail.com>
2024-07-29 20:58:18 +0530

    add

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 import time
 from bs4 import BeautifulSoup
 import os
-from flask import Flask, jsonify
+from flask import Flask, render_template, jsonify
 
 # Configure WebDriver options if needed
 chrome_options = Options()
@@ -16,19 +16,17 @@ def getCFData(username):
     driver.get(f"https://codeforces.com/profile/{username}")
     data = driver.page_source
 
-    with open ("data.txt", "w", encoding="utf-8") as f:
+    with open ("codeforces.txt", "w", encoding="utf-8") as f:
         f.write(data)
 
     data1 = None
-    with open("data.txt", "r", encoding="utf-8") as f:
-        data1 = f.read()
-
-    time.sleep(2)
+    with open("codeforces.txt", "r", encoding="utf-8") as f:
+        datacf = f.read()
 
     cf_data = {}
 
     # Parse the HTML content with BeautifulSoup
-    soup = BeautifulSoup(data1, 'html.parser')
+    soup = BeautifulSoup(datacf, 'html.parser')
 
     # Extract relevant data
     username_element = soup.find('a', class_="rated-user user-gray")
@@ -61,20 +59,69 @@ def getCFData(username):
     # Convert the dictionary to a JSON string
     return cf_data
     driver.quit()
+def getLCData(username):
+    LCData = {}
+    driver = webdriver.Chrome()  # Make sure chromedriver is in your PATH
+    driver.get(f"https://leetcode.com/{username}")
+    time.sleep(3)
+    data = driver.page_source
+    with open ("leetcode.txt", "w", encoding="utf-8") as f:
+        f.write(data)
+    soup = BeautifulSoup(data, 'html.parser')
+    problems = soup.find_all('span', class_="text-[30px] font-semibold leading-[32px]")[0].text
+    try:
+        rating = soup.find_all('div', class_="text-label-1 dark:text-dark-label-1 flex items-center text-2xl")[0].text
+    except:
+        rating = "N/A"
+    total_active_days = soup.find_all('span', class_="font-medium text-label-2 dark:text-dark-label-2")[0].text
+    max_streak = soup.find_all('span', class_="font-medium text-label-2 dark:text-dark-label-2")[1].text
+    images = soup.find_all('img')
+    images = images[2:-1]
+    badges = []
+    for img in images:
+        badges.append(img['alt'])
+    types = soup.find_all('div', class_="text-sd-foreground text-xs font-medium");
+    easy = types[0].text
+    medium = types[1].text
+    hard = types[2].text
+    rank = soup.find('span', class_="ttext-label-1 dark:text-dark-label-1 font-medium").text
+    rank = rank.replace(",", "")
+    # print(problems, rating, total_active_days, max_streak)
+    # print(badges)
+    # print(easy, medium, hard, rank)
+    LCData['username'] = username
+    LCData['problems'] = problems
+    LCData['rating'] = rating
+    LCData['total_active_days'] = total_active_days
+    LCData['max_streak'] = max_streak
+    LCData['badges'] = badges
+    LCData['easy'] = easy
+    LCData['medium'] = medium
+    LCData['hard'] = hard
+    LCData['rank'] = rank
+    return LCData
+    driver.quit()
+
+    
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Welcome to Codeforces API</h1><p>use /api/codeforces/your username</p>"
+    return "<h1>Welcome to Code API</h1><h3>use /api/codeforces/&lt;username&gt; for Codeforces</h3><h3>use /api/leetcode/&lt;username&gt; for Leetcode</h3>"
 
 @app.route('/api/codeforces/<username>', methods=['GET'])
-def get(username):
+def getCF(username):
     cf_data = getCFData(username)
     cf_data['username'] = username
     return jsonify(cf_data)
 
-if __name__ == "__main__":  # Ensure you use the PORT variable
-    app.run(host="0.0.0.0")
+@app.route('/api/leetcode/<username>', methods=['GET'])
+def getLC(username):
+    LCData = getLCData(username)
+    LCData['username'] = username
+    return jsonify(LCData)
 
-# Close the WebDriver
+if __name__ == "__main__":
+    app.run(debug=True)
+
